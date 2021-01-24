2976cb344e3648f800d4c00a0c03342166abe26d
Melody Winter <littlemissantivirus@protonmail.com>
2021-01-23 23:15:26 -0400

    :bulb: Comment out emoji code.
    Comment out the code that prevents dwm from rendering coloured emojis, that is fixed in libxft-bgra.

--- drw.c
+++ drw.c
@@ -140,11 +140,11 @@ xfont_create(Drw *drw, const char *fontname, FcPattern *fontpattern)
 	 * https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=916349
 	 * and lots more all over the internet.
 	 */
-	FcBool iscol;
+	/* FcBool iscol;
 	if(FcPatternGetBool(xfont->pattern, FC_COLOR, 0, &iscol) == FcResultMatch && iscol) {
 		XftFontClose(drw->dpy, xfont);
 		return NULL;
-	}
+	} */ // This being commented requires libxft-bgra to be installed, so that dwm can render coloured emoji.
 
 	font = ecalloc(1, sizeof(Fnt));
 	font->xfont = xfont;
